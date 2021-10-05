adf1e068af724cd8bbb5203599c860b1e1d83df4
jack <jackzhoumine@gmail.com>
2021-10-05 23:44:53 +0800

    全局api变更

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,28 @@ import App from './App.vue'
 import { setupRouter } from './route/index'
 
 const app = createApp(App)
+
+// 全局配置
+// app.config.compilerOptions.isCustomElement
+// 全局属性
+app.config.globalProperties.appName = 'my vue3 demo'
+
+function plugin() {
+  console.log('vue plugin')
+}
+
+app.use(plugin)
+
+// 全局混入
+app.mixin({
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+})
+
+// 注册全局组件
+app.component('globalComponent', {})
+// app.directive('') // 全局指令
+
 setupRouter(app)
 app.mount('#app')
