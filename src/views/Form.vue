e9276673370fe1a2ea492d2ad195f94989cbe639
jack <jackzhoumine@gmail.com>
2021-10-05 11:02:55 +0800

    reactive

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
     <h1>form</h1>
+    <h2>reactive</h2>
+    <p>count:{{ count }}</p>
+    <p>doubleCount:{{ doubleCount }}</p>
+    <p><button @click="increase">加一</button></p>
   </div>
 </template>
 <script lang="ts">
-import { ref, reactive, watch, computed } from 'vue'
+import { ref, reactive, watch, computed, toRefs } from 'vue'
+type dataPropsType = {
+  count: number
+  doubleCount: number
+  increase: () => void
+}
 export default {
   name: 'Form',
   components: {},
   setup(props, { emit, attrs, slots }) {
-    return {}
+    const dataProps: dataPropsType = reactive({
+      count: 0,
+      increase: () => dataProps.count++,
+      doubleCount: computed(() => dataProps.count * 2),
+    })
+    return {
+      ...toRefs(dataProps),
+    }
   },
 }
 </script>
