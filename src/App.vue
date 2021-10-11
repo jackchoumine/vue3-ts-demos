5e51b16f46150ada7dd57d43d7384695f67fa414
jack <jackzhoumine@gmail.com>
2021-10-11 22:51:06 +0800

    style: 修改格式错误

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,12 @@
   <div id="app">
     <!-- <NotificationProvider> -->
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/form">Form Validation</router-link> |
-      <router-link to="/posts">Paginated Posts</router-link> |
-      <router-link to="/images">Infinite Images</router-link> |
-      <router-link to="/upload">Upload Dropzone</router-link>
+      <RouterLink to="/">Home</RouterLink> | <RouterLink to="/form">Form Validation</RouterLink> |
+      <RouterLink to="/posts">Paginated Posts</RouterLink> |
+      <RouterLink to="/images">Infinite Images</RouterLink> |
+      <RouterLink to="/upload">Upload Dropzone</RouterLink>
     </div>
-    <router-view />
+    <RouterView />
     <!-- </NotificationProvider> -->
   </div>
 </template>
