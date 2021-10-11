97a40ec8e7020bc4934be3723334c847c25af9d3
jack <jackzhoumine@gmail.com>
2021-10-11 22:47:25 +0800

    chore: 声明全局类型

---
+++ types.d.ts
@@ -0,0 +1,8 @@
+/*
+ * @Description : 全局类型声明
+ * @Date        : 2021-10-11 22:38:13 +0800
+ * @Author      : JackChou
+ * @LastEditTime: 2021-10-11 22:39:24 +0800
+ * @LastEditors : JackChou
+ */
+declare type AnyObject = Record<string, unknown>
