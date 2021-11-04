c190d5bc63b0c7d3ce5f7e122704ab4a86b2b292
jack <jackzhoumine@gmail.com>
2021-11-05 00:17:55 +0800

    泛型组件

--- src/views/Examples.vue
+++ src/views/Examples.vue
@@ -2,7 +2,7 @@
  * @Description :
  * @Date        : 2021-10-27 22:05:30 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-28 02:38:43 +0800
+ * @LastEditTime: 2021-11-04 23:59:46 +0800
  * @LastEditors : JackChou
 -->
 <template>
@@ -11,6 +11,7 @@
     <DemoV3 />
     <MyInput v-model="input" />
     <p>{{ input }}</p>
+    <Foo :data="[{ a: 'a', b: 'b', c: 'c' }]" keys='' />
   </div>
 </template>
 <script setup>
@@ -18,6 +19,7 @@ import { ref, reactive } from 'vue'
 import DemoV2 from '../components/DemoV2.vue'
 import DemoV3 from '../components/DemoV3.vue'
 import MyInput from '../components/MyInput.vue'
+import { Foo } from '../components/jsx/Foo'
 const input = ref('hello')
 </script>
 <style scoped lang="scss"></style>
